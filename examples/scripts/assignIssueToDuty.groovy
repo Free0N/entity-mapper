8e3a659a394612ef5e37ac571bcab0ecdc711b83
Pavel Afanasev <afanasev.p@gmail.com>
2026-02-19 19:22:08 +0800

    Добавил пример скрипта, использующего Entity Mapper

---
+++ examples/scripts/assignIssueToDuty.groovy
@@ -0,0 +1,26 @@
+import org.samearch.jira.lib.entity.mapper.api.EntityMapper
+
+import com.atlassian.jira.project.Project
+import com.atlassian.jira.user.util.UserManager
+import com.atlassian.jira.user.ApplicationUser
+
+@WithPlugin("org.samearch.jira.lib.entity-mapper-plugin")
+@PluginModule
+EntityMapper entityMapper
+@PluginModule
+UserManager userManager
+
+Project project = issue.getProjectObject()
+if (project == null || project.isArchived()) {
+    return
+}
+
+String projectKey = project.key
+String projectDutyKey = "project.${projectKey}.duty"
+entityMapper.getMappedValue(projetDutyKey)
+    .ifPresent { dutyUserLogin ->
+        ApplicationUser dutyUser = userManager.getUserByName(dutyUserLogin)
+        if (dutyUser != null && dutyUser.active) {
+            issue.setAssignee(dutyUser)
+        }
+    }
